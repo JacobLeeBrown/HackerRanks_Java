f8d0776dcf95fe7ef9366123d5f38f4bb1e27e8f
Jacob Brown <jlbrown5@illinois.edu>
2021-04-25 17:08:56 -0500

    Initial pom and binary tree code

--- src/main/java/binarytree/BinaryTreeMain.java
+++ src/main/java/binarytree/BinaryTreeMain.java
@@ -1,2 +1,151 @@
-package binarytree;public class BinaryTreeMain {
+package binarytree;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class BinaryTreeMain {
+
+    /* Design a naive integer BinaryTree where:
+     *  1. Each subtree tracks its mean value
+     *  2. Method to insert an element (if not already in tree) and returns if tree changed
+     *  3. Method to remove an element (if in tree) and returns if tree changes
+     *  4. Methods to access any subtree and get the subtree's mean
+     */
+
+    private BinaryTree theTree;
+    private ArrayList<Integer> elems = new ArrayList<>();
+
+    public BinaryTreeMain() {}
+
+    public boolean addElem(int n) {
+        if (elems.isEmpty()) {
+            theTree = new BinaryTree(n);
+            elems.add(n);
+            return true;
+        }
+        else if (elems.contains(n)) {
+            return false; // Do nothing -> element is already in tree
+        }
+
+        elems.add(n);
+        theTree.addElem(n);
+        return true;
+    }
+
+    public boolean removeElem(int n) {
+        if (elems.isEmpty()) {
+            return false;
+        }
+        else if (!elems.contains(n)) {
+            return false; // Do nothing -> element is not in tree
+        }
+
+        elems.remove(n);
+        theTree.removeElem(n);
+        return true;
+    }
+
+    public int getNodes() {
+        if (elems.isEmpty()) return 0;
+        return theTree.nodes;
+    }
+
+    public float getMean() {
+        if (elems.isEmpty()) return 0;
+        return theTree.getMean();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class BinaryTree {
+
+        public int root;
+        public int sum;
+        public int nodes;
+
+        public BinaryTree leftChild;
+        public BinaryTree rightChild;
+
+        public BinaryTree(int n) {
+            root = n;
+            sum = n;
+            nodes = 1;
+        }
+
+        public void addElem(int n) {
+            if (n < root) {
+                if (Objects.isNull(leftChild)) {
+                    leftChild = new BinaryTree(n);
+                }
+                else {
+                    leftChild.addElem(n);
+                }
+            }
+            else { // n > root -> can't be equal due to outside checks
+                if (Objects.isNull(rightChild)) {
+                    rightChild = new BinaryTree(n);
+                }
+                else {
+                    rightChild.addElem(n);
+                }
+            }
+            nodes += 1;
+            sum += n;
+        }
+
+        // Returns whether or not the tree is empty, indicating to the parent to clear it
+        public boolean removeElem(int n) {
+            if (n == root) {
+                int newRoot = 0;
+                boolean leftNull = Objects.isNull(leftChild);
+                boolean rightNull = Objects.isNull(rightChild);
+                if (leftNull && rightNull) {
+                    return true;
+                }
+                else if (leftNull) {
+                    newRoot = rightChild.root;
+                    boolean isEmpty = rightChild.removeElem(newRoot);
+                    if (isEmpty) rightChild = null;
+                }
+                else { // else right is or is not null, doesn't matter since we're being naive
+                    newRoot = leftChild.root;
+                    boolean isEmpty = leftChild.removeElem(newRoot);
+                    if (isEmpty) leftChild = null;
+                }
+                root = newRoot;
+            }
+            else if (n < root) {
+                boolean isEmpty = leftChild.removeElem(n);
+                if (isEmpty) leftChild = null;
+            }
+            else { // n > root
+                boolean isEmpty = rightChild.removeElem(n);
+                if (isEmpty) rightChild = null;
+            }
+            nodes -= 1;
+            sum -= n;
+            return false;
+        }
+
+        public int getDepth() {
+            boolean leftNull = Objects.isNull(leftChild);
+            boolean rightNull = Objects.isNull(rightChild);
+            if (leftNull && rightNull) {
+                return 1;
+            }
+            int leftDepth = leftNull ? 0 : leftChild.getDepth();
+            int rightDepth = rightNull ? 0 : rightChild.getDepth();
+
+            return leftDepth > rightDepth ? 1 + leftDepth : 1 + rightDepth;
+        }
+
+        public float getMean() {
+            return ((float) sum) / nodes;
+        }
+
+    }
+
 }
+
--- src/test/java/binarytree/BinaryTreeMainTest.java
+++ src/test/java/binarytree/BinaryTreeMainTest.java
@@ -1,4 +1,50 @@
+package binarytree;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BinaryTreeMainTest {
-  
+
+    private BinaryTreeMain target;
+
+    @Test
+    public void testEmptyTree() {
+        target = new BinaryTreeMain();
+
+        assertEquals(0, target.getNodes());
+        assertEquals(0, target.getMean());
+    }
+
+    @Test
+    public void testAddInitialElem() {
+        target = new BinaryTreeMain();
+
+        boolean treeChanged = target.addElem(5);
+
+        assertTrue(treeChanged);
+        assertEquals(1, target.getNodes());
+        assertEquals(5, target.getMean());
+    }
+
+    @Test
+    public void testAddSeveralElems() {
+        target = new BinaryTreeMain();
+
+        boolean treeChanged1 = target.addElem(5);
+        boolean treeChanged2 = target.addElem(2);
+        boolean treeChanged3 = target.addElem(8);
+        boolean treeChanged4 = target.addElem(1);
+        boolean treeChanged5 = target.addElem(3);
+        boolean treeChanged6 = target.addElem(7);
+        boolean treeChanged7 = target.addElem(9);
+
+        assertTrue(treeChanged1 && treeChanged2 && treeChanged3 && treeChanged4 && treeChanged5 && treeChanged6 && treeChanged7);
+
+        assertEquals(7, target.getNodes());
+
+        float expectedMean = ((float) (1 + 2 + 3 + 5 + 7 + 8 + 9)) / 7f;
+        assertEquals(expectedMean, target.getMean());
+    }
+
 }
